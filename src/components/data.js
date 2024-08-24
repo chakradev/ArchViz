249c2ffc68f0d13de499fe56883777073e02a372
Sidan <101472457+sidan54@users.noreply.github.com>
2024-08-24 13:01:18 +0530

    Add files via upload

---
+++ src/components/data.js
@@ -0,0 +1,43 @@
+// data.js
+export const modelPaths = {
+    office: '/models/office/scene.gltf',
+    room: '/models/room/scene.gltf',
+    restaurant: '/models/restaurant/scene.gltf',
+  };
+  
+  export const markerPositions = [
+    [1.5, -0.8, 0],
+    [1.5, -0.8, -3.5],
+    [-1.5, -0.8, -3.5],
+    [-1.5, -0.8, 0.5],
+    [-1.5, -0.8, 3.5],
+    [2, -1.5, 2],
+  ];
+  
+  export const markerLabels = [
+    "2. Table",
+    "3. Office 1",
+    "5. Office 2",
+    "4. Conference hall",
+    "6. Washroom",
+    "1. Main Room",
+  ];
+  
+  export const markerInfos = [
+    "ⓘ Info \n-----------------------\nThis is a table used for meetings.",
+    "ⓘ Info \n-----------------------\nOffice 1 is for the manager.",
+    "ⓘ Info \n-----------------------\nOffice 2 is for the team lead.",
+    "ⓘ Info \n-----------------------\nThe conference hall can seat up to 50 people.",
+    "ⓘ Info \n-----------------------\nThe washroom is located here.",
+    "ⓘ Info \n-----------------------\nThe main room serves as the reception area.",
+  ];
+  
+  export const cameraPositions = [
+    [2.5, 0, 3],
+    [3, -0.8, -4.5],
+    [-6, 0, -3],
+    [-5, 0, 2],
+    [-4, 0, 6],
+    [0, 0, 6],
+  ];
+  
