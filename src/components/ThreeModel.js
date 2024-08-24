6e69d34b2317930cd08c6013a75c93419aaf092d
Sidan <101472457+sidan54@users.noreply.github.com>
2024-08-24 13:02:49 +0530

    Update ThreeModel.js

--- src/components/ThreeModel.js
+++ src/components/ThreeModel.js
@@ -4,58 +4,19 @@ import { OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
-import ClickableSphere from './ClickableSphere'; // Importing the ClickableSphere component
-import LoadingSpinner from './LoadingSpinner'; // Importing the LoadingSpinner component
+import ClickableSphere from './ClickableSphere';
+import LoadingSpinner from './LoadingSpinner';
+import { modelPaths, markerPositions, markerLabels, markerInfos, cameraPositions } from './data';
 
 const ThreeModel = ({ modelType, setLoading }) => {
   const group = useRef();
   const [selected, setSelected] = useState(null);
   const [cameraTarget, setCameraTarget] = useState(null);
 
-  const modelPath = {
-    office: '/models/office/scene.gltf',
-    room: '/models/room/scene.gltf',
-    restaurant: '/models/restaurant/scene.gltf',
-  }[modelType];
+  const modelPath = modelPaths[modelType];
 
   const gltf = useLoader(GLTFLoader, modelPath);
   const grassTexture = useLoader(TextureLoader, '/textures/terr.jpg');
-
-  const markerPositions = [
-    [1.5, -0.8, 0],
-    [1.5, -0.8, -3.5],
-    [-1.5, -0.8, -3.5],
-    [-1.5, -0.8, 0.5],
-    [-1.5, -0.8, 3.5],
-    [2, -1.5, 2],
-  ];
-
-  const markerLabels = [
-    "2. Table",
-    "3. Office 1",
-    "5. Office 2",
-    "4. Conference hall",
-    "6. Washroom",
-    "1. Main Room",
-  ];
-
-  const markerInfos = [
-    "ⓘ Info \n-----------------------\nThis is a table used for meetings.",
-    "ⓘ Info \n-----------------------\nOffice 1 is for the manager.",
-    "ⓘ Info \n-----------------------\nOffice 2 is for the team lead.",
-    "ⓘ Info \n-----------------------\nThe conference hall can seat up to 50 people.",
-    "ⓘ Info \n-----------------------\nThe washroom is located here.",
-    "ⓘ Info \n-----------------------\nThe main room serves as the reception area.",
-  ];
-
-  const cameraPositions = [
-    [2.5, 0, 3],
-    [3, -0.8, -4.5],
-    [-6, 0, -3],
-    [-5, 0, 2],
-    [-4, 0, 6],
-    [0, 0, 6],
-  ];
 
   useEffect(() => {
     if (!gltf) return;
