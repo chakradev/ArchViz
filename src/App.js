cf69c4e91255425fbe0a5012bc731346120136d1
Sidan <101472457+sidan54@users.noreply.github.com>
2024-09-09 17:52:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import ThreeCanvas from './components/ThreeModel'; // Ensure the import is correct
+import ThreeCanvas from './components/ThreeCanvas'; // Ensure the import is correct
 import './App.css';
 
 const App = () => {
